5892c55486bfb945efd18a0dac8b8f90f6b34d54
Brahma Dev <me@brahma.world>
2019-04-20 14:52:21 +0530

    Added support for BigInt

--- index.js
+++ index.js
@@ -1,66 +1,98 @@
 module.exports = (function(Base85) {
-	var DEFAULT_CHARACTER_SET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~";
-	var s, val, Base85Chars;
+	let DEFAULT_CHARACTER_SET =
+		"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~";
+	let s, val, Base85Chars;
 
 	Base85.encode = function(integer) {
-		s = '';
+		//Send integer as string if bigger than 2^53
+		if (typeof f != "bigint") {
+			integer = BigInt(integer);
+		}
+		s = "";
 		while (integer > 0) {
 			s = Base85.characterSet[integer % Base85.indexLength] + s;
-			integer = Math.floor(integer / Base85.indexLength);
+			integer = integer / Base85.indexLength;
 		}
-		return s !== '' ? s : '0';
+		return s !== "" ? s : "0";
 	};
-	
-	
-	Base85.encodeSigned = function(bytes){
-		var MSB = "0x80",  SUB="0x100";
-		for(bytes;bytes>1;bytes--){
+
+	Base85.encodeSigned = function(bytes) {
+		if (bytes % 4 != 0) {
+			throw Error("Signed values can only be in multiple of 4 bytes");
+		}
+		let MSB = "0x80",
+			SUB = "0x100";
+		for (let b = bytes; b > 1; b--) {
 			MSB = MSB + "00";
 			SUB = SUB + "00";
 		}
-		SUB = parseInt(SUB);
-		MSB = parseInt(MSB);
-		return function(integer) {
-			if(integer < 0){
-				integer=integer+SUB;
+		let PAD = SUB;
+		SUB = BigInt(SUB);
+		MSB = BigInt(MSB);
+		return function(integer, padding = true) {
+			//Send integer as string if bigger than 2^53
+			if (typeof f != "bigint") {
+				integer = BigInt(integer);
 			}
-			return Base85.encode(integer);
+			if (integer >= MSB) {
+				throw Error(`Number too big for ${bytes} bytes.`);
+			}
+			if (integer <= -MSB) {
+				throw Error(`Number too big for ${bytes} bytes.`);
+			}
+			if (integer < 0) {
+				integer = integer + SUB;
+			}
+			if (padding) {
+				let l = (bytes / 4) * 5;
+				return (PAD + Base85.encode(integer)).substr(-l, l);
+			} else {
+				return Base85.encode(integer);
+			}
 		};
-	}
-	
+	};
+
 	Base85.decode = function(Base85String) {
-		val = 0;
+		val = BigInt(0);
 		Base85Chars = Base85String.split("").reverse();
 		Base85Chars.forEach(function(character, index) {
-			val += Base85.characterSet.indexOf(character) * Math.pow(Base85.indexLength, index);
+			val +=
+				BigInt(Base85.characterSet.indexOf(character)) *
+				Base85.indexLength ** BigInt(index);
 		});
 		return val;
 	};
 
-	Base85.decodeSigned = function(bytes){
-		var MSB = "0x80",  SUB="0x100";
-		for(bytes;bytes>1;bytes--){
+	Base85.decodeSigned = function(bytes) {
+		if (bytes % 4 !== 0) {
+			throw Error("Signed values can only be in multiple of 4 bytes");
+		}
+		let MSB = "0x80",
+			SUB = "0x100";
+		for (let b = bytes; b > 1; b--) {
 			MSB = MSB + "00";
 			SUB = SUB + "00";
 		}
-		SUB = parseInt(SUB);
-		MSB = parseInt(MSB);
-
-		//var SUB = 0x100 << ((bytes-1)*8);
+		SUB = BigInt(SUB);
+		MSB = BigInt(MSB);
+		//let SUB = 0x100 << ((bytes-1)*8);
 		return function(Base85String) {
 			val = Base85.decode(Base85String);
 			//https://stackoverflow.com/a/13468626
+			if (val >= SUB) {
+				throw Error(`Number too big for ${bytes} bytes.`);
+			}
 			if (val > MSB) {
 				val = val - SUB;
-			}else if(val === MSB){
-				return -0;
+			} else if (val == MSB) {
+				return BigInt(-0);
 			}
 			return val;
 		};
-	}
+	};
 
 	Base85.setCharacterSet = function(chars) {
-		var arrayOfChars = chars.split(""),
+		let arrayOfChars = chars.split(""),
 			uniqueCharacters = [];
 
 		arrayOfChars.forEach(function(char) {
@@ -70,7 +102,7 @@ module.exports = (function(Base85) {
 				throw Error("You must use unique characters.");
 			}
 		});
-		Base85.indexLength = arrayOfChars.length;
+		Base85.indexLength = BigInt(arrayOfChars.length);
 		Base85.characterSet = arrayOfChars;
 	};
 
--- test/index.js
+++ test/index.js
@@ -9,74 +9,129 @@ describe('Base85', function() {
 		res.should.equal("0");
 		done();
 	});
-	
+
 	it("should return Sw?xI", function(done) {
 		var res = Base85.encode(1497790883);
 		res.should.equal("Sw?xI");
 		done();
 	});
-	
+
+	it("should return d*-h_{Y}sg", function(done) {
+		var res = Base85.encode("9223372036854775807");
+		res.should.equal("d*-h_{Y}sg");
+		done();
+	});
+
+	it("should return error ", function(done) {
+		should.throws(function() {
+			var encoder = Base85.encodeSigned(8);
+			encoder("9223372036854775808");
+		}, /Number too big for/, "Did not throw message.");
+		done();
+	});
+
+	it("should return error ", function(done) {
+		should.throws(function() {
+			var encoder = Base85.encodeSigned(8);
+			encoder("-9223372036854775808");
+		}, /Number too big for/, "Did not throw message.");
+		done();
+	});
+
+	it("should return K&@H;m", function(done) {
+		var res = Base85.encode(92233720368);
+		res.should.equal("K&@H;m");
+		done();
+	});
+
+	it("should return 0000K&@H;m", function(done) {
+		var encoder = Base85.encodeSigned(8);
+		var res = encoder(92233720368);
+		res.should.equal("0000K&@H;m");
+		done();
+	});
+
+	it("should return _sv#aG}{?@", function(done) {
+		var encoder = Base85.encodeSigned(8);
+		var res = encoder("-9223372036854");
+		res.should.equal("_sv#aG}{?@");
+		done();
+	});
+
 	it("should return |NsC0", function(done) {
 		var encoder = Base85.encodeSigned(4);
 		var res = encoder(-1);
 		res.should.equal("|NsC0");
 		done();
 	});
-	
+
 	it("should return 0", function(done) {
 		var encoder = Base85.encodeSigned(4);
 		var res = encoder(0);
-		res.should.equal("0");
+		res.should.equal("00000");
 		done();
 	});
-	
+
 	it("should return fB*mi", function(done) {
 		var encoder = Base85.encodeSigned(4);
 		var res = encoder(-2147483647);
 		res.should.equal("fB*mi");
 		done();
 	});
-	
+
 	it("should return 19936", function(done) {
 		var res = Base85.decode("2$k");
-		res.should.equal(19936);
+		res.should.equal(BigInt(19936));
 		done();
 	});
 	it("should return 2147483647", function(done) {
 		var res = Base85.decode("fB*mg");
-		res.should.equal(2147483647);
+		res.should.equal(BigInt(2147483647));
 		done();
 	});
-	
+
 	it("should return -0", function(done) {
 		var decoder = Base85.decodeSigned(4);
 		var res = decoder("fB*mh");
-		res.should.equal(-0);
+		res.should.equal(BigInt(-0));
 		done();
 	});
-	
+
 	it("should return -1", function(done) {
 		var decoder = Base85.decodeSigned(4);
 		var res = decoder("|NsC0");
-		res.should.equal(-1);
+		res.should.equal(BigInt(-1));
 		done();
 	});
-	
+
 	it("should return 1", function(done) {
 		var decoder = Base85.decodeSigned(4);
 		var res = decoder("1");
-		res.should.equal(1);
+		res.should.equal(BigInt(1));
 		done();
 	});
-	
-	
+
 	it("should return -2147483647", function(done) {
 		var decoder = Base85.decodeSigned(4);
 		var res = decoder("fB*mi");
-		res.should.equal(-2147483647);
+		res.should.equal(BigInt(-2147483647));
 		done();
 	});
-	
+
+	it("should return 9223372036854775807", function(done) {
+		var decoder = Base85.decodeSigned(8);
+		var res = decoder("d*-h_{Y}sg");
+		res.should.equal(BigInt("9223372036854775807"));
+		done();
+	});
+
+	it("should return -9223372036854775807", function(done) {
+		var decoder = Base85.decodeSigned(8);
+		var res = decoder("d*-h_{Y}si");
+		res.should.equal(BigInt("-9223372036854775807"));
+		done();
+	});
+
 	it("should return rmnO - base62 ", function(done) {
 		Base85.setCharacterSet("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 		var res = Base85.encode(6520900);
